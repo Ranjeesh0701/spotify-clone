d4f10df27fc7e9934b196b8c03c97f9fdb085213
Ranjeesh0701 <techstack0701@gmail.com>
2023-06-20 21:36:44 +0530

    spotify-clone

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["dzerscosdayaqwpfogyo.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
